a8ebd7af1f88c489b1b008c21dae744f1a639811
William Furr <william.furr@gmail.com>
2016-12-19 17:40:14 -0500

    Add reward formula, number formatting

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ function doGet() {
   return HtmlService.createHtmlOutputFromFile('calc.html');
 }
 
+var MAX_REWARD = 50 * 1e6;
+
 /**
  *  Calculate the value, volume, reward, and type of hauler for a given
  *  inventory block represented by an evepraisal URL.
@@ -23,11 +25,11 @@ function calculateReward(evepraisal_url) {
   //
   var fitsIn = function(value, volume) {
     var hauler_limits = [ // Ordered by value and volume from least to greatest
-      { type: 'Interceptor', value: 100000000, volume: 100 },
-      { type: 'T1 Industrial', value: 100000000, volume: 30000 },
-      { type: 'Blockade Runner', value: 500000000, volume: 9000 },
-      { type: 'Deep Space Transport', value: 500000000, volume: 50000 },
-      { type: 'Orca', value: 2000000000, volume: 90000 },
+      { type: 'Interceptor',          value:  100 * 1e6, volume: 100 },
+      { type: 'T1 Industrial',        value:  100 * 1e6, volume: 30000 },
+      { type: 'Blockade Runner',      value:  500 * 1e6, volume: 9000 },
+      { type: 'Deep Space Transport', value:  500 * 1e6, volume: 55000 },
+      { type: 'Orca',                 value: 2000 * 1e6, volume: 90000 },
     ];
     for (var i = 0; i < hauler_limits.length; i++) {
       var hauler = hauler_limits[i];
@@ -43,9 +45,7 @@ function calculateReward(evepraisal_url) {
     value: evepraisal.totals.sell,
     volume: evepraisal.totals.volume,
     hauler_type: fitsIn(evepraisal.totals.sell, evepraisal.totals.volume),
-    reward: 20000000 // TODO calculate reward based on hauler type and fraction of max cargo
-    // reward formula: MAX_REWARD*max(Volume/60,000, collateral/500,000,000)
-    // e.g. MAX_REWARD 50M
+    reward: MAX_REWARD * Math.max(evepraisal.totals.volume / 60000,
+                                  evepraisal.totals.sell / (500 * 1e6))
   };
 }
-
